9fb53574fb14483b7fc6efcfa3616b49c212a14f
Benoît Rouleau <benoit.rouleau@icloud.com>
2025-09-21 18:45:40 -0400

    Update bgone to 0.2.0

--- bgone.rb
+++ bgone.rb
@@ -1,14 +1,14 @@
 class Bgone < Formula
   desc "Ultra-fast CLI tool for removing solid background colors from images"
   homepage "https://github.com/benface/bgone"
-  version "0.1.0"
+  version "0.2.0"
 
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/benface/bgone/releases/download/v#{version}/bgone-v#{version}-x86_64-apple-darwin.tar.gz"
-    sha256 "042dfd2474e515b83eb9b6fcba234a70884ca7938b87ca460ae60cc121eb0566"
+    sha256 "df35dd84e27036c638ac86fb8a0047ea32ad6e69bdd5e161f0535a95e91e5717"
   elsif OS.mac? && Hardware::CPU.arm?
     url "https://github.com/benface/bgone/releases/download/v#{version}/bgone-v#{version}-aarch64-apple-darwin.tar.gz"
-    sha256 "c51f781c0c2134a2dae6423360a08cba6bf8096f3856186f7d75e942703aea93"
+    sha256 "f8f219607522b5b8071d52d853864d31f090424bf70e622be9317c50156732b4"
   elsif OS.linux? && Hardware::CPU.intel?
     url "https://github.com/benface/bgone/releases/download/v#{version}/bgone-x86_64-unknown-linux-gnu.tar.gz"
     sha256 ""  # Will be filled after creating release
